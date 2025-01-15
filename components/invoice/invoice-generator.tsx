44bd9ef8d029bcf1afb468f4f789dd77f5132279
Mantasha Zubair <98265444+MantashaZubair@users.noreply.github.com>
2025-01-15 13:21:56 +0530

    Update invoice-generator.tsx

--- components/invoice/invoice-generator.tsx
+++ components/invoice/invoice-generator.tsx
@@ -24,7 +24,7 @@ export default function InvoiceGenerator({ invoiceId }: { invoiceId?: string }) 
       if (invoiceId) {
         try {
           const response = await axios.get<InvoiceData>(
-            `${process.env.server}/api/v1/invoice/invoices/${invoiceId}`,
+            `${process.env.NEXT_PUBLIC_SERVER}/api/v1/invoice/invoices/${invoiceId}`,
             { withCredentials: true }
           );
           setInitialData(response.data);
