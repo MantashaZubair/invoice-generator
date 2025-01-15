9add6a6af3d920661bb2a232b2f588524420b547
Mantasha Zubair <98265444+MantashaZubair@users.noreply.github.com>
2025-01-15 12:43:34 +0530

    Update page.tsx

--- app/user/setting/edit/page.tsx
+++ app/user/setting/edit/page.tsx
@@ -24,7 +24,7 @@ export default function Edit(){
     const generateInvoiceNumber = async () => {
       try {
         const response = await axios.get(
-          `http://localhost:8002/api/v1/invoice/invoices/userId/${user?.user._id}`
+          `${process.env.server}/api/v1/invoice/invoices/userId/${user?.user._id}`
         );
         const invoices = response.data; // Assuming this is an array
         console.log(invoices);
