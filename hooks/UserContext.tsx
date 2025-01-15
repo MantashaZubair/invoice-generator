13e52bbe81c828b1dc32cabe564d865d87e5ef57
Mantasha Zubair <98265444+MantashaZubair@users.noreply.github.com>
2025-01-15 13:24:40 +0530

    Update UserContext.tsx

--- hooks/UserContext.tsx
+++ hooks/UserContext.tsx
@@ -27,7 +27,7 @@ export const UserProvider = ({ children }:{children: React.ReactNode}) => {
   const fetchUserProfile = async () => {
     try {
       // const accessToken = Cookies.get("accessToken");
-      const response = await axios.get(`${process.env.server}/api/v1/user/me`, {
+      const response = await axios.get(`${process.env.NEXT_PUBLIC_SERVER}/api/v1/user/me`, {
         // headers: {
         //   Authorization: `Bearer ${accessToken}`,
         // },
