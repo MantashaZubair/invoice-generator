d50e06a23d35ae3b54869a9cbd0a131abcbfcffe
Mantasha Zubair <98265444+MantashaZubair@users.noreply.github.com>
2025-01-15 12:37:01 +0530

    Update use-invoice.ts

--- hooks/use-invoice.ts
+++ hooks/use-invoice.ts
@@ -132,8 +132,8 @@ export function useInvoice(initialData?: InvoiceData) {
       try {
         const isEditing = !!initialData;
         const url = isEditing 
-          ? `http://localhost:8002/api/v1/invoice/invoices/${initialData._id}`
-          : "http://localhost:8002/api/v1/invoice/invoices";
+          ? `${process.env.server}/api/v1/invoice/invoices/${initialData._id}`
+          : `${process.env.server}/api/v1/invoice/invoices`;
         
         const method = isEditing ? 'put' : 'post';
 
@@ -229,7 +229,7 @@ export function useInvoice(initialData?: InvoiceData) {
 
     try {
       const response = await axios.get(
-        `http://localhost:8002/api/v1/invoice/invoices/userId/${user.user._id}`
+        `${process.env.server}/api/v1/invoice/invoices/userId/${user.user._id}`
       );
       const invoices = response.data;
       let newInvoiceNumber = "INV-0001";
